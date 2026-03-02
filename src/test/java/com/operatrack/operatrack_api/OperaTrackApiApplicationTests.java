c901d27b7e247456eae63f7e63599b61ce9526ff
Daniel Aranda <daniel_aranda@homedepot.com>
2026-03-02 14:09:14 -0600

    chore: add initial configuration

---
+++ src/test/java/com/operatrack/operatrack_api/OperaTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operatrack.operatrack_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperaTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
